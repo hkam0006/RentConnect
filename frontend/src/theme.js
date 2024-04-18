43b7883be8eb4c5f5b4c48152f39c524c63c0ade
Sonny Kam <sonnykam18@gmail.com>
2024-04-18 23:36:42 +1000

    Added global theme file

---
+++ frontend/src/theme.js
@@ -0,0 +1,22 @@
+import {createTheme} from "@mui/material"
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    background: {
+      default: "#F2F2F2"
+    },
+    primary: {
+      main: "#5c4db1"
+    }
+  },
+  typography: {
+    fontFamily: 'Lato, sans-serif',
+    button: {
+      textTransform: "unset",
+      fontWeight: 700
+    }
+  }
+})
+
+export default theme
