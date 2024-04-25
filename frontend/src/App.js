6e081d9477c3675c50bfdd1a2bfef425a3f5b9f3
Jiawei Liao <jiaweiliao01@gmail.com>
2024-04-25 12:06:52 +1000

    added basic routes for application details and properties

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,13 +9,33 @@ import PropertyDetails  from './components/property_page/PropertyDetails';
 import {CssBaseline, ThemeProvider} from "@mui/material"
 
 function App() {
-  return <>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-
-      <Properties />
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/ApplicationDetails/:applicationId' element={<ApplicationDetails />} />
+            {/* Route for the button */}
+            <Route path='/Properties' element={<Properties />} />
+          </Routes>
+        </Router>
     </ThemeProvider>
-  </>
+  )
+}
+
+function Home() {
+  return (
+    <div>
+      <Link to="/ApplicationDetails/testID">
+        <button>Go to ApplicationDetails with ID "testID"</button>
+      </Link>
+      <Link to="/Properties">
+        <button>Go to Properties</button>
+      </Link>
+    </div>
+    
+  );
 }
 
 export default App;
