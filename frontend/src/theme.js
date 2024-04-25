200f905736ff55670445db815b49552f4c460868
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-04-25 11:07:49 +1000

    Removed font import in themes

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -10,16 +10,8 @@ const theme = createTheme({
       main: "#5c4db1"
     }
   },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: `@font-face {
-        font-family: "Lato, sans-serif";
-        src: url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
-      }`
-    }
-  },
   typography: {
-    fontFamily: 'Lato, sans-serif',
+    fontFamily: 'Roboto, sans serif',
     button: {
       textTransform: "unset",
       fontWeight: 700
