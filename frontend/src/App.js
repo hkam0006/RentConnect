4192aeee7d8b97c6c3405b4c9fb45456f7abde1b
Jiawei Liao <jiaweiliao01@gmail.com>
2024-04-25 11:47:39 +1000

    added very basic routing from main page to applicationdetails

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import ApplicationDetails from './components/applications/ApplicationDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Routes>
+        <Route path='/ApplicationDetails/:applicationId' element={<ApplicationDetails />} />
+        {/* Route for the button */}
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <Link to="/ApplicationDetails/testID">
+        <button>Go to ApplicationDetails with ID "testID"</button>
+      </Link>
     </div>
   );
 }
