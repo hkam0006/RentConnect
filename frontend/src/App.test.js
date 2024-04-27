56617eaca144b83630dfcea13f87734bcc41b7ac
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-04-28 00:24:23 +1000

    Fixing test stage

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -5,6 +5,11 @@ test('TextEncoder is globally defined in Jest', () => {
   expect(global.TextEncoder).toBeDefined();
 });
 
+test('TextDecoder is globally defined in Jest', () => {
+  expect(global.TextDecoder).toBeDefined();
+});
+
+
 test('renders app without crashing', () => {
   render(<App />)
 });
