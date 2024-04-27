4bae7c6c5013d2df8d08cd2b4710213b4419bf03
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-04-28 00:15:57 +1000

    Fixing test stage in CI

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+beforeAll(async () => {
+  global.TextEncoder = require('util').TextEncoder;
+  global.TextDecoder = require('util').TextDecoder;
+});
+
 test('renders app without crashing', () => {
   render(<App />)
 });
