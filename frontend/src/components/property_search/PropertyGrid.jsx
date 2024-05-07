6d0e97c1d31b807def2e3ab00f27fb0f5ca65cfb
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-05-07 23:21:07 +1000

    Added no properties component

--- frontend/src/components/property_search/PropertyGrid.jsx
+++ frontend/src/components/property_search/PropertyGrid.jsx
@@ -12,6 +12,13 @@ import { Bookmark, FilterList, Search } from '@mui/icons-material';
 import ListingImage from '../property_page/listing.jpg'
 import ListingImageTwo from '../property_page/listing2.jpg'
 
+const NoPropertiesFound = () => {
+  return <Stack justifyContent={'center'} textAlign={'center'} mt={5}>
+    <Typography variant='h6'>We couldn't find any properties that meet your search parameters.</Typography>
+    <Typography variant='subtitle'>Please try adjusting your filters or search again.</Typography>
+  </Stack>
+}
+
 export default function PropertySearch() {
 
   const [properties, setProperties] = useState([]);
@@ -45,59 +52,57 @@ export default function PropertySearch() {
         />
         <Button variant='contained'><FilterList /></Button>
       </Stack>
-      <Grid container spacing={3} mt={1}>
-        {
-          properties.map((property) => <Grid key={property.propert_id} item sx={12} sm={4} height="fit-content">
-            <Card >
-              <CardMedia>
-                <Carousel autoPlay={false} indicators={false} swipe={true} fullHeightHover={true} animation={"slide"} navButtonsAlwaysVisible cycleNavigation={false}>
-                  <ImgElement src={ListingImage} alt='Stock Listing Image' sx={{ width: "100%", objectFit: "cover" }} />
-                  <ImgElement src={ListingImageTwo} alt='Stock Listing Image' sx={{ width: "100%" }} />
-                  <ImgElement src={ListingImageTwo} alt='Stock Listing Image' sx={{ width: "100%" }} />
-                  <ImgElement src={ListingImageTwo} alt='Stock Listing Image' sx={{ width: "100%" }} />
-                  <ImgElement src={ListingImageTwo} alt='Stock Listing Image' sx={{ width: "100%" }} />
-                </Carousel>
-              </CardMedia>
-              <CardContent>
-                <Stack direction='row' justifyContent='space-between' alignItems={'center'}>
-                  <Typography variant='body' >
-                    {fullAddress(
-                      property.propert_street_number,
-                      property.property_street_name,
-                      property.property_street_type,
-                      property.property_suburb,
-                      property.property_state
-                    )}
-                  </Typography>
-                  <IconButton size='small' aria-label='bookmark' >
-                    <Bookmark />
-                  </IconButton>
-                </Stack>
-                <Stack direction='row' spacing={2}>
-                  <Stack direction={'row'} spacing={1} alignItems={"center"}>
-                    <Stack direction='row' spacing={0.5} alignItems={"center"}>
-                      <BedIcon />
-                      <Typography alignContent="center" fontWeight={700} variant='h6'>{property.property_bedroom_count}</Typography>
-                    </Stack>
-                    <Divider orientation='vertical' />
-                    <Stack direction='row' spacing={0.5} alignItems={"center"}>
-                      <BathtubIcon />
-                      <Typography alignContent="center" fontWeight={700} variant='h6'>{property.property_bathroom_count}</Typography>
-                    </Stack>
-                    <Divider orientation='vertical' />
-                    <Stack direction='row' spacing={0.5} alignItems={"center"}>
-                      <DirectionsCarIcon />
-                      <Typography alignContent="center" fontWeight={700} variant='h6'>{property.property_car_spot_count}</Typography>
-                    </Stack>
-                    <Divider orientation='vertical' />
-                    <Typography variant='subtitle'>{property.propertyType}</Typography>
+      {properties.length > 0 ? <Grid container spacing={3} mt={1}>
+        {properties.map((property) => <Grid key={property.property_id} item sx={12} sm={4}>
+          <Card >
+            <CardMedia>
+              <Carousel autoPlay={false} indicators={false} swipe={true} fullHeightHover={true} animation={"slide"} navButtonsAlwaysVisible cycleNavigation={false}>
+                <ImgElement src={ListingImage} alt='Stock Listing Image' sx={{ width: "100%", objectFit: "cover" }} />
+                <ImgElement src={ListingImageTwo} alt='Stock Listing Image' sx={{ width: "100%" }} />
+                <ImgElement src={ListingImageTwo} alt='Stock Listing Image' sx={{ width: "100%" }} />
+                <ImgElement src={ListingImageTwo} alt='Stock Listing Image' sx={{ width: "100%" }} />
+                <ImgElement src={ListingImageTwo} alt='Stock Listing Image' sx={{ width: "100%" }} />
+              </Carousel>
+            </CardMedia>
+            <CardContent>
+              <Stack direction='row' justifyContent='space-between' alignItems={'center'}>
+                <Typography variant='body' >
+                  {fullAddress(
+                    property.property_street_number,
+                    property.property_street_name,
+                    property.property_street_type,
+                    property.property_suburb,
+                    property.property_state
+                  )}
+                </Typography>
+                <IconButton size='small' aria-label='bookmark' >
+                  <Bookmark />
+                </IconButton>
+              </Stack>
+              <Stack direction='row' spacing={2}>
+                <Stack direction={'row'} spacing={1} alignItems={"center"}>
+                  <Stack direction='row' spacing={0.5} alignItems={"center"}>
+                    <BedIcon />
+                    <Typography alignContent="center" fontWeight={700} variant='h6'>{property.property_bedroom_count}</Typography>
                   </Stack>
+                  <Divider orientation='vertical' />
+                  <Stack direction='row' spacing={0.5} alignItems={"center"}>
+                    <BathtubIcon />
+                    <Typography alignContent="center" fontWeight={700} variant='h6'>{property.property_bathroom_count}</Typography>
+                  </Stack>
+                  <Divider orientation='vertical' />
+                  <Stack direction='row' spacing={0.5} alignItems={"center"}>
+                    <DirectionsCarIcon />
+                    <Typography alignContent="center" fontWeight={700} variant='h6'>{property.property_car_spot_count}</Typography>
+                  </Stack>
+                  <Divider orientation='vertical' />
+                  <Typography variant='subtitle'>{property.propertyType}</Typography>
                 </Stack>
-              </CardContent>
-            </Card>
-          </Grid>)
-        }
-      </Grid>
+              </Stack>
+            </CardContent>
+          </Card>
+        </Grid>)}
+      </Grid> : <NoPropertiesFound />}
     </Container >
   )
 }
