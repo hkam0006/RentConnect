2a28c1f8f08d533d2a5e3253062c8b4b9906ae44
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-05-07 21:36:00 +1000

    Added supabase/supabase-js to dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ApplicationDetails from './components/applications/ApplicationDetails';
-import './App.css';
 import theme from './theme';
 import Properties from './components/property_page/Properties';
 import PropertyDetails  from './components/property_page/PropertyDetails';
