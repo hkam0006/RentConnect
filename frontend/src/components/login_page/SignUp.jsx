fda516656f6970eaba3223dda690ed7c8b4c08e3
spencermcnamara <smcn0012@student.monash.edu>
2024-08-05 16:37:06 +1000

    Added functionality to sign up

--- frontend/src/components/login_page/SignUp.jsx
+++ frontend/src/components/login_page/SignUp.jsx
@@ -4,7 +4,7 @@ import TextField from '@mui/material/TextField';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import Lock from '@mui/icons-material/Lock';
 import { Button} from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { supabase } from "../../supabase";
 import { useState} from 'react';
 
@@ -17,24 +17,77 @@ function SignUp(){
     const handlePasswordChange = f => {
         setPassword(f.target.value);
     }
+    const [passwordConf, setPasswordConf] = useState('');
+    const handlePasswordConfChange = f => {
+        setPasswordConf(f.target.value);
+    }
+    const [errorTextFlag, setErrorTextFlag] = useState(false);
     const [errorText, setErrorText] = useState(false);
     const navigate = useNavigate();
   return (
     <Box sx={{ml: '35%', mt: '10%', backgroundColor: 'white', width: '30%', mb: '10%', borderRadius: 2}}>
         <br/>
-        <Box sx={{fontSize: '220%', fontWeight: 'bold', ml: '7%'}}>Log In</Box>
+        <Box sx={{fontSize: '220%', fontWeight: 'bold', ml: '7%'}}>Sign Up</Box>
         <Box sx={{ display: 'flex', alignItems: 'flex-end', ml: "5%"}}>
             <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5, alignSelf: 'bottom', height: '35px', width: '35px'}} />
-            <TextField id='Email' value={email} onChange={handleEmailChange} label='Email' variant='standard' error={errorText} sx={{width: '80%'}} inputProps={{sx: {height: '15%', fontSize: '130%'}}} InputLabelProps={{sx: {fontSize: '130%'}}}/>
+            <TextField id='Email' value={email} onChange={handleEmailChange} label='Email' variant='standard' sx={{width: '80%'}} inputProps={{sx: {height: '15%', fontSize: '130%'}}} InputLabelProps={{sx: {fontSize: '130%'}}}/>
         </Box>
+        <br/>
         <Box sx={{ display: 'flex', alignItems: 'flex-end', ml: "5%"}}>
             <Lock sx={{ color: 'action.active', mr: 1, my: 0.5, alignSelf: 'bottom', height: '35px', width: '35px'}} />
-            <TextField id='Password' value={password} onChange={handlePasswordChange} label='Password' variant='standard' type='password' error={errorText} helperText={errorText?'Incorrect email or password':''} sx={{width: '80%'}} inputProps={{sx: {height: '15%', fontSize: '130%'}}} InputLabelProps={{sx: {fontSize: '130%'}}}/>
+            <TextField id='Password' value={password} onChange={handlePasswordChange} label='Password' variant='standard' type='password' error={errorTextFlag} helperText={errorTextFlag?errorText:''} sx={{width: '80%'}} inputProps={{sx: {height: '15%', fontSize: '130%'}}} InputLabelProps={{sx: {fontSize: '130%'}}}/>
+        </Box>
+        <br/>
+        <Box sx={{ display: 'flex', alignItems: 'flex-end', ml: "5%"}}>
+            <Lock sx={{ color: 'action.active', mr: 1, my: 0.5, alignSelf: 'bottom', height: '35px', width: '35px'}} />
+            <TextField id='PasswordConf' value={passwordConf} onChange={handlePasswordConfChange} label='Confirm Password' variant='standard' type='password' error={errorTextFlag} sx={{width: '80%'}} inputProps={{sx: {height: '15%', fontSize: '130%'}}} InputLabelProps={{sx: {fontSize: '130%'}}}/>
         </Box>
         <br/>
         <Box sx={{display: 'flex', justifyContent: 'center'}}>
             <Button variant="contained" onClick={() => {
-                    supabase.auth.signInWithPassword({
+                if (password != passwordConf){   
+                    setErrorText('Passwords must match')
+                    setErrorTextFlag(true);
+                    setPassword('');
+                    setPasswordConf('');
+                    return
+                }
+                var valid = false
+                if (password.length > 7){
+                    var contains_numbers = false;
+                    var contains_caps = false;
+                    var contains_special = false;
+                    let numbers = '1234567890';
+                    let caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                    let special = '!@#$%^&*()-_=+[{]}|:;,<.>/?'
+                    for (var i = 0; i < password.length; i++){
+                        if (!contains_numbers && numbers.includes(password[i])){ 
+                            contains_numbers = true;
+                        } 
+                        else{
+                            if (!contains_caps && caps.includes(password[i])){ 
+                                contains_caps = true;
+                            } 
+                            else{
+                                if (!contains_special && special.includes(password[i])){
+                                    contains_special = true;
+                                }
+                            } 
+                        } 
+                    }
+                    if(contains_numbers && contains_caps && contains_special){
+                        valid = true
+                    }
+                }
+                if(!valid){
+                    setErrorText('Passwords must be 8 characters or more, contain a capital, number and a special character')
+                    setErrorTextFlag(true);
+                    setPassword('');
+                    setPasswordConf('');
+                    return;
+                }
+                else{
+                    supabase.auth.signUp({
                         email: email,
                         password: password,
                       }).then(data=> {
@@ -42,22 +95,21 @@ function SignUp(){
                             navigate('/dashboard');
                         }
                         else{
-                            setErrorText(true);
+                            setErrorText('Account already exists')
+                            setErrorTextFlag(true);
                             setPassword('');
+                            setPasswordConf('');
                         }
                     })
                     .catch(error => {
-                        setErrorText(true);
+                        setErrorText(`Unhandled error: ${error}`)
+                        setErrorTextFlag(true);
                         setPassword('');
+                        setPasswordConf('');
                         console.log(error);
                     })
-                }}>Sign In</Button>
-        </Box>
-        <br/>
-        <Box sx={{display: 'flex', justifyContent: 'center'}}>
-            <Box sx={{mr:'2%'}}>Don't have an account?</Box>
-            <Link to="/SignUp">Sign Up</Link>
-        
+                }
+            }}>Create Account</Button>
         </Box>
         <br/>
     </Box>
