184d882017c1278c472ac2e5deb3df06acb48db8
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-08-30 09:43:56 +1000

    Added property detials modal

--- frontend/src/public-components/PublicPropertyPage.jsx
+++ frontend/src/public-components/PublicPropertyPage.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import { Box, Grid, Container, Card, CardMedia, CardContent, Typography, Button, Skeleton, Paper, InputBase,Divider, IconButton, Chip, Menu, MenuItem, AppBar, Toolbar, Stack} from '@mui/material';
+import { Box, Modal, Grid, Container, Card, CardMedia, CardContent, Typography, Button, Skeleton, Paper, InputBase,Divider, IconButton, Chip, Menu, MenuItem, AppBar, Toolbar, Stack, Dialog, DialogTitle, DialogContent} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import HouseIcon from '@mui/icons-material/House';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import SearchIcon from '@mui/icons-material/Search';
 import { supabase } from '../supabase';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import BathtubIcon from '@mui/icons-material/Bathtub';
+import KingBedIcon from '@mui/icons-material/KingBed';
+import AspectRatioIcon from '@mui/icons-material/AspectRatio';
+import CloseIcon from '@mui/icons-material/Close';
 
 const fullAddress = (p) => {
   return `${p.property_street_number} ${p.property_street_name}, ${p.property_suburb}, ${p.property_state} ${p.property_postcode}`;
 };
 
-const PropertyCard = ({loading, property}) => {
+const PropertyCard = ({loading, property, showModal}) => {
   const navigate = useNavigate();
+  console.log(property)
   return <>
     {!loading ? <Card sx={{ borderRadius: 0, boxShadow: 2 }}>
       <CardMedia
@@ -36,9 +42,7 @@ const PropertyCard = ({loading, property}) => {
           color="primary" 
           fullWidth 
           sx={{ mt: 2 }} 
-          onClick={() => {
-            navigate("/LogIn")
-          }}
+          onClick={showModal}
         >
           View Details
         </Button>
@@ -135,13 +139,13 @@ const SkeletonProperties = () => {
   </>
 }
 
-const ActualProperties = ({properties}) => {
+const ActualProperties = ({properties, setShowModal}) => {
   return (
     <>
       {properties.map((p) => {
         return (
           <Grid key={p.property_id} item xs={12} sm={6} md={4}>
-            <PropertyCard key={p.property_id} loading={false} property={p}/>
+            <PropertyCard key={p.property_id} loading={false} property={p} showModal={() => setShowModal(p)}/>
           </Grid>
         )
       })}
@@ -200,12 +204,89 @@ const Navbar = () => {
   );
 }
 
+const PropertyDetailsModal = ({open, onClose}) => {
+  const property = open;
+  const navigate = useNavigate();
+  return (
+    <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
+      <DialogTitle>
+        {property.property_type} in {property.property_suburb}
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{ position: 'absolute', right: 8, top: 8 }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent dividers>
+        <CardMedia
+          component="img"
+          height="400"
+          image={property.property_pictures[0]}
+          alt={property.property_id}
+        />
+        <Box sx={{ mt: 2 }}>
+          <Typography variant="h6" color="text.secondary" gutterBottom>
+            <LocationOnIcon sx={{ mr: 1 }} />
+            {fullAddress(property)}
+          </Typography>
+          <Typography variant="h5" color="primary" gutterBottom>
+            ${property.property_rent} {property.property_rent_frequency}
+          </Typography>
+        </Box>
+
+        <Grid container spacing={4} sx={{ mt: 2 }}>
+          <Grid item xs={12} md={8}>
+            <Typography variant="h6" component="h2" gutterBottom>
+              Property Description
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              {property.property_description}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box>
+              <Typography variant="h6" component="h2" gutterBottom>
+                Features
+              </Typography>
+              <Typography variant="body2" gutterBottom>
+                <KingBedIcon sx={{ mr: 1 }} />
+                {property.property_bedroom_count} Bedrooms
+              </Typography>
+              <Typography variant="body2" gutterBottom>
+                <BathtubIcon sx={{ mr: 1 }} />
+                {property.property_bathroom_count} Bathrooms
+              </Typography>
+              <Typography variant="body2" gutterBottom>
+                <AspectRatioIcon sx={{ mr: 1 }} />
+                {property.property_footprint}
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+
+        <Box sx={{ mt: 4, textAlign: 'center' }}>
+          <Button variant="contained" color="primary" size="large" onClick={() => navigate('/LogIn')}>
+            Apply Now!
+          </Button>
+        </Box>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
 const PublicPropertyPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState("")
   const [chips, setChips] = useState([searchParams.get("q")])
+  const [showDetails, setShowDetails] = useState(null)
   const [properties, setProperties] = useState([])
+
+  const setShowModal = (property) => {
+    setShowDetails(property)
+  }
 
   const handleFilterSubmit = () => {
     setChips((prev) => [...prev, search])
@@ -237,6 +318,7 @@ const PublicPropertyPage = () => {
   }, [chips])
 
   return (<>
+    { Boolean(showDetails) && <PropertyDetailsModal open={showDetails} onClose={() => setShowDetails(null)}/>}
     <Box sx={{height: "100vh", pb: 1}}>
       <Navbar />
       <Container>
@@ -247,7 +329,7 @@ const PublicPropertyPage = () => {
         />  
         <ChipContainer chips={chips} setChips={setChips}/>
         <Grid container spacing={2} mt={1}>
-          {loading  ? <SkeletonProperties /> : <ActualProperties properties={properties}/>}
+          {loading  ? <SkeletonProperties /> : <ActualProperties properties={properties} setShowModal={setShowModal}/>}
           {properties.length === 0 && !loading && <Stack sx={{textAlign: "center", width: "100%", mt: 3}}>
               <Typography variant='h6'>Can't find any properties that match your query</Typography>
             </Stack>
