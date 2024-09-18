cbe36a00101e1d8bd7026e04cbd4f9aff7258ec7
Lionsluke <lphi0011@student.monash.edu>
2024-09-18 15:05:17 +1000

    added dynamic name and no applications message to renter home

--- frontend/src/renter-components/renter_home/ApplicationsTable.jsx
+++ frontend/src/renter-components/renter_home/ApplicationsTable.jsx
@@ -1,5 +1,5 @@
 import {
-    Button, Chip,
+    Button, Chip, Paper,
     Stack,
     Table,
     TableBody,
@@ -21,6 +21,7 @@ import {useNavigate} from "react-router-dom";
 import useGetPropertyByPropertyID from "../../queries/Property/useGetPropertyByPropertyID";
 import useGetPropertiesByPropertyIDs from "../../queries/Property/useGetPropertiesByPropertyIDs";
 import AppLoader from "../../manager-components/property_page/AppLoader";
+import {px} from "style-value-types";
 
 const fullAddress = (number, name, type, suburb, state) => {
     return `${number} ${name} ${type}, ${suburb}, ${state}`
@@ -72,72 +73,80 @@ export default function ApplicationsTable(applications) {
 
     if (loading) return <AppLoader />
 
-    return (
-        <Table stickyHeader sx={{minWidth: 650}} aria-label="Table of properties">
-            <TableHead>
-                <TableRow>
-                    <StyledTableCell><Typography fontSize={"12px"} fontWeight={700}>Property </Typography></StyledTableCell>
-                    <StyledTableCell align="right"><Typography fontWeight={700} fontSize={"12px"}>Application Status</Typography></StyledTableCell>
-                    <StyledTableCell align="right"><Typography fontWeight={700} fontSize={"12px"}></Typography></StyledTableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {applications.applications?.map((row, index) => (
-                    <TableRow
-                        key={index}
-                        sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                    >
-                        <TableCell width={{width: "fit-content"}}>
-                            {/* <Card sx={{ padding: 2, }} > */}
-                            <Typography variant='body' fontWeight={700}>
-                                {fullAddress(
-                                    properties[index].property_street_number,
-                                    properties[index].property_street_name,
-                                    properties[index].property_street_type,
-                                    properties[index].property_suburb,
-                                    properties[index].property_state
-                                )}
-                            </Typography>
-                            <Stack direction='row' spacing={2} justifyContent="start" sx={{width: "fit-content"}}>
-                                <ImgElement sx={{height: '150px', width: '264px', borderRadius: 3}}
-                                            src={properties[index].property_pictures[0]} alt='Stock Listing Image'/>
-                                <Stack>
-                                    <Stack direction='row' spacing={2}>
-                                        <Stack direction='row' spacing={0.5} alignItems={"center"}>
-                                            <BedIcon/>
-                                            <Typography alignContent="center" fontWeight={700}
-                                                        variant='h6'>{properties[index].property_bedroom_count}</Typography>
-                                        </Stack>
-                                        <Stack direction='row' spacing={0.5} alignItems={"center"}>
-                                            <BathtubIcon/>
-                                            <Typography alignContent="center" fontWeight={700}
-                                                        variant='h6'>{properties[index].property_bathroom_count}</Typography>
-                                        </Stack>
-                                        <Stack direction='row' spacing={0.5} alignItems={"center"}>
-                                            <DirectionsCarIcon/>
-                                            <Typography alignContent="center" fontWeight={700}
-                                                        variant='h6'>{properties[index].property_car_spot_count}</Typography>
-                                        </Stack>
-                                    </Stack>
-                                    <Typography>${properties[index].price} {properties[index].property_rent_frequency}</Typography>
-                                    <Typography>Type: {properties[index].property_type}</Typography>
-                                    <Typography>Available: {properties[index].property_lease_start}</Typography>
-                                </Stack>
-                            </Stack>
-                            {/* </Card> */}
-                        </TableCell>
-                        <TableCell align="right">
-                            <ApplicationStatusChip appStatus={row.application_status}/>
-                        </TableCell>
-                        <TableCell align="right">
-                            <Stack spacing={1}>
-                                <Button variant='contained'
-                                        onClick={() => navigate(`/application/${properties[index].property_id}`)}>View</Button>
-                            </Stack>
-                        </TableCell>
+    if (applications.applications?.length <= 0) {
+        return (
+            <Paper elevation={0} sx={{mt: 0, borderRadius: 3, height: '150px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <Typography fontWeight={'bold'} fontSize={'16pt'}>No applications found.</Typography>
+            </Paper>
+        )
+    } else {
+        return (
+            <Table stickyHeader sx={{minWidth: 650}} aria-label="Table of properties">
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell><Typography fontSize={"12px"} fontWeight={700}>Property </Typography></StyledTableCell>
+                        <StyledTableCell align="right"><Typography fontWeight={700} fontSize={"12px"}>Application Status</Typography></StyledTableCell>
+                        <StyledTableCell align="right"><Typography fontWeight={700} fontSize={"12px"}></Typography></StyledTableCell>
                     </TableRow>
-                ))}
-            </TableBody>
-        </Table>
-    )
+                </TableHead>
+                <TableBody>
+                    {applications.applications?.map((row, index) => (
+                        <TableRow
+                            key={index}
+                            sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                        >
+                            <TableCell width={{width: "fit-content"}}>
+                                {/* <Card sx={{ padding: 2, }} > */}
+                                <Typography variant='body' fontWeight={700}>
+                                    {fullAddress(
+                                        properties[index].property_street_number,
+                                        properties[index].property_street_name,
+                                        properties[index].property_street_type,
+                                        properties[index].property_suburb,
+                                        properties[index].property_state
+                                    )}
+                                </Typography>
+                                <Stack direction='row' spacing={2} justifyContent="start" sx={{width: "fit-content"}}>
+                                    <ImgElement sx={{height: '150px', width: '264px', borderRadius: 3}}
+                                                src={properties[index].property_pictures[0]} alt='Stock Listing Image'/>
+                                    <Stack>
+                                        <Stack direction='row' spacing={2}>
+                                            <Stack direction='row' spacing={0.5} alignItems={"center"}>
+                                                <BedIcon/>
+                                                <Typography alignContent="center" fontWeight={700}
+                                                            variant='h6'>{properties[index].property_bedroom_count}</Typography>
+                                            </Stack>
+                                            <Stack direction='row' spacing={0.5} alignItems={"center"}>
+                                                <BathtubIcon/>
+                                                <Typography alignContent="center" fontWeight={700}
+                                                            variant='h6'>{properties[index].property_bathroom_count}</Typography>
+                                            </Stack>
+                                            <Stack direction='row' spacing={0.5} alignItems={"center"}>
+                                                <DirectionsCarIcon/>
+                                                <Typography alignContent="center" fontWeight={700}
+                                                            variant='h6'>{properties[index].property_car_spot_count}</Typography>
+                                            </Stack>
+                                        </Stack>
+                                        <Typography>${properties[index].price} {properties[index].property_rent_frequency}</Typography>
+                                        <Typography>Type: {properties[index].property_type}</Typography>
+                                        <Typography>Available: {properties[index].property_lease_start}</Typography>
+                                    </Stack>
+                                </Stack>
+                                {/* </Card> */}
+                            </TableCell>
+                            <TableCell align="right">
+                                <ApplicationStatusChip appStatus={row.application_status}/>
+                            </TableCell>
+                            <TableCell align="right">
+                                <Stack spacing={1}>
+                                    <Button variant='contained'
+                                            onClick={() => navigate(`/application/${properties[index].property_id}`)}>View</Button>
+                                </Stack>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        )
+    }
 }
--- frontend/src/renter-components/renter_home/RenterHome.jsx
+++ frontend/src/renter-components/renter_home/RenterHome.jsx
@@ -11,18 +11,20 @@ import AppLoader from "../../manager-components/property_page/AppLoader";
 import useGetApplicationsByRenterID from "../../queries/Application/useGetApplicationsByRenterID";
 import useGetPropertiesByCompanyID from "../../queries/Property/useGetPropertiesByCompanyID";
 import useGetUserID from "../../queries/useGetUserID";
+import useGetRenterByRenterID from "../../queries/Renter/useGetRenterByRenterID";
 
 export default function RenterHome() {
 
     const {userID, loading: userLoading} = useGetUserID();
+    const {renter, loading: renterLoading} = useGetRenterByRenterID(userID);
     const {applications, loading: applicationsLoading} = useGetApplicationsByRenterID(userID);
 
-    if (userLoading || applicationsLoading) return <AppLoader />
+    if (userLoading || renterLoading || applicationsLoading) return <AppLoader />
 
     return (
         <NavigationMenu>
             <div style={{padding: "20px", marginTop: "64px"}}>
-                <Typography variant='h4' fontWeight={700} color="text.primary">Welcome User!</Typography>
+                <Typography variant='h4' fontWeight={700} color="text.primary">Welcome {renter[0].renter_first_name}!</Typography>
                 <Typography variant='h6' fontWeight={700} color="text.secondary">You have 2 new messages.</Typography>
 
                 <Paper sx={{ mt: 2, borderRadius: 3 }} elevation={3}>
