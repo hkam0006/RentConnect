3c2ff930530a5dcd461c74d9a9b3fa35ae9022fb
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-10-09 15:52:35 +1100

    Fixing CI CD pipeline

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('@mui/x-charts/BarChart', () => ({
+  BarChart: () => null, // Return a simple functional component that renders nothing
+}));
+
 test('renders app without crashing', () => {
   render(<App />)
 });
