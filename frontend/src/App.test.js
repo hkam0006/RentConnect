0ae103443ad1ac4440f939ecac1b8c020aa9710b
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-10-09 15:44:34 +1100

    Fixing CI CD pipeline

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,10 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import Properties from './manager-components/property_page/Properties';
-
-const { TextEncoder, TextDecoder } = require('util');
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
 
 test('renders app without crashing', () => {
   render(<App />)
