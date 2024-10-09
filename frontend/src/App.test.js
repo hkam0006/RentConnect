66099c333b223f67c7841f86b35dc35f8d0e9ae9
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-10-09 16:09:15 +1100

    Fixing CI CD pipeline

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,3 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from './utils/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -5,6 +14,18 @@ jest.mock('@mui/x-charts/BarChart', () => ({
   BarChart: () => null, // Return a simple functional component that renders nothing
 }));
 
+let persistor = persistStore(store)
+
 test('renders app without crashing', () => {
-  render(<App />)
+  render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <Router>
+          <PersistGate persistor={persistor}>
+            <App />
+          </PersistGate>
+        </Router>
+      </Provider>
+  </React.StrictMode>
+  )
 });
