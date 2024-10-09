f2dec1adbcf27961d9f2768bf23e40213b01b5d2
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-10-09 15:23:10 +1100

    Fixing CI CD pipeline

--- frontend/src/jest.config.js
+++ frontend/src/jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   setupFiles: ['./jest.polyfills.js'],
+  transformIgnorePatterns: [
+    "/node_modules/(?!(d3-shape|d3-[a-z-]+)/)", // Transform d3-shape and related d3 modules
+  ],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest", // Use babel-jest for transforming files
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"], 
 }
