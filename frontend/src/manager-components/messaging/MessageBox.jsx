67390c1d260976a89ccf69ec61d6f9a3b8b04754
Jiawei Liao <jiaweiliao01@gmail.com>
2024-10-11 18:09:33 +1100

    pressing enter auto sends

--- frontend/src/manager-components/messaging/MessageBox.jsx
+++ frontend/src/manager-components/messaging/MessageBox.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { Paper, TextField, Button, Box } from '@mui/material'
 
 function MessageBox({ message, setMessage, HandleMessagesPush }) {
+    function handleKeyPress(e) {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault()
+            HandleMessagesPush()
+        }
+    }
+
     return (
         <Paper sx={{ padding: 2, margin: 2, width: '100%' }}>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
@@ -11,6 +18,7 @@ function MessageBox({ message, setMessage, HandleMessagesPush }) {
                     label='message'
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}
+                    onKeyDown={handleKeyPress}
                     fullWidth
                     multiline
                     rows={1}
